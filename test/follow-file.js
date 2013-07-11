eb889fa67260040949677c7cfda6a73e1b736513
Hugo LEHMANN <shogi31@gmail.com>
2013-07-11 17:05:57 +0200

    Edit unit test

--- test/follow-file.js
+++ test/follow-file.js
@@ -6,36 +6,37 @@ var log = require('debug')('test')
 
 var ff = require('../index.js')
 
-describe('Follow', function () {
+describe('Follow', function() {
 
   var tmpPath = path.join(__dirname, 'tmp.log');
 
   beforeEach(function(done) {
-    var stream = fs.createWriteStream(tmpPath, {flags: 'w'})
-    log('started')
-    writeRemaining(stream, 10, done);
+    writeRemaining(tmpPath, 'w', 10, done);
   })
 
   after(function(done) {
     fs.unlink(tmpPath, done);
   })
 
-  function writeRemaining(stream, count, cb){
-    log('appending')
-    for (var i = 0; i < count; i++){
-      stream.write('Jun 16 10:20:58 server.hostname coreaudiod[121]: Enabled automatic stack shots because audio IO is inactive ' + i + ' \n', 'utf-8')
+  function writeRemaining(path, flags, count, cb) {
+    var stream = fs.createWriteStream(tmpPath, {flags: flags});
+    log('writing');
+    for(var i = 0; i < count; i++) {
+      stream.write('Jun 16 10:20:58 server.hostname coreaudiod[121]: Enabled automatic stack shots because audio IO is inactive ' +
+        i + ' \n', 'utf-8')
     }
-    stream.once('drain',cb);
+    stream.end();
+    stream.once('close', cb);
   }
 
-  it('should read through the file when it starts', function (done) {
+  it('should read through the file when it starts', function(done) {
 
     var lines = 0
 
-    ff(tmpPath, {start: 8}).on('data', function (line) {
+    ff(tmpPath, {start: 8}).on('data', function(line) {
       log('line', line)
       lines++
-      if (lines === 8) {
+      if(lines === 8) {
         this.destroy();
         done();
       }
@@ -43,74 +44,75 @@ describe('Follow', function () {
 
   })
 
-  it('should follow updates to the file', function (done) {
-
-    var stream = fs.createWriteStream(tmpPath, {flags: 'a'})
+  it('should follow updates to the file', function(done) {
 
     log('started')
     var lines = -1
     var totalLines = 80
 
-    ff(tmpPath, {start: 1}).on('data', function (line) {
+    ff(tmpPath, {start: 1}).on('data',function(line) {
       log('lines++', lines++, line)
-      if (lines === totalLines) {
+      if(lines === totalLines) {
         this.destroy();
         done()
       }
     }).once('data', function() {
-        writeRemaining(stream, totalLines, function(){
+        writeRemaining(tmpPath, 'a', totalLines, function() {
           log('finished extra write')
         })
       })
   })
 
-  it('should follow a file even if it is deleted', function(done){
+  it('should follow a file even if it is deleted', function(done) {
 
     var lines = 0
     var totalLines = 25
     var deleted = false
-    ff(tmpPath, {start: 5}).on('data', function (line) {
+    ff(tmpPath, {start: 5}).on('data', function(line) {
       log('lines++', lines++, line)
-      if (lines >= totalLines) {
+      if(lines >= totalLines) {
         log('all done')
         this.destroy();
         done()
       }
 
-      if (lines === 5 && !deleted){
+      if(lines === 5 && !deleted) {
         deleted = true
-        log('delete file')
-        fs.unlink(tmpPath, function(){
+        log('delete')
+        fs.unlink(tmpPath, function() {
           log('create file')
-          var stream = fs.createWriteStream(tmpPath, {flags: 'w'})
-          writeRemaining(stream, 20, function(){
-            log('finished')
-          })
+          setTimeout(function() {
+            writeRemaining(tmpPath, 'w', 20, function() {
+              log('finished');
+            })
+          }, 2)
         })
       }
     })
   })
 
-  it('should follow a file even if it is emptied', function(done){
+  it('should follow a file even if it is erase', function(done) {
 
     var lines = 0
     var totalLines = 25
-    var empty = false
-    ff(tmpPath, {start: 5}).on('data', function (line) {
+    var erase = false
+    ff(tmpPath, {start: 5}).on('data', function(line) {
       log('lines++', lines++, line)
-      if (lines >= totalLines) {
+      if(lines >= totalLines) {
         log('all done')
         this.destroy();
         done()
       }
 
-      if (lines === 5 && !empty){
-        empty = true
-        log('empty file')
-        var stream = fs.createWriteStream(tmpPath, {flags: 'w'})
-        writeRemaining(stream, 20, function(){
-          log('finished')
-        })
+      if(lines === 5 && !erase) {
+        erase = true
+        log('erase file')
+        var stream = fs.createWriteStream(tmpPath, {flags: 'w'});
+        setTimeout(function() {
+          writeRemaining(tmpPath, 'a', 20, function() {
+            log('finished')
+          })
+        }, 2)
       }
     })
   })
